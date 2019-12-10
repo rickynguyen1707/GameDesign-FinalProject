c5f4287744d2b6817ff465c768b2a169456d798a
Ricky <nguyenkhanglhp1605@gmail.com>
2019-12-09 22:57:55 -0500

    Transform snowball rotation, Tweek snow rate

--- Assets/Scripts/Snowball.cs
+++ Assets/Scripts/Snowball.cs
@@ -7,6 +7,10 @@ public class Snowball : MonoBehaviour
     private float moveSpeed = 7f;
     private GameObject target;
     private Vector2 moveDirection;
+    private Vector3 targetPosition;
+    private Vector3 dir;
+    private bool facingRight = false;
+    private float angle;
     Rigidbody2D rgbd;
 
     
@@ -18,6 +22,25 @@ public class Snowball : MonoBehaviour
         moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
         rgbd.velocity = new Vector2(moveDirection.x, moveDirection.y);
         Destroy(gameObject, 5f);
+        //Transform snowball direction to follow player
+        targetPosition = target.transform.position;
+        dir = targetPosition - this.transform.position;
+        angle = Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        /*if (Vector3.Distance(target.transform.position, transform.position) < 20)
+        {
+            if (target.transform.position.x > transform.position.x && !facingRight)// if the target is to the right of enemy and enemy is not facing right
+                Flip();
+            if (target.transform.position.x < transform.position.x && facingRight)
+                Flip();
+        }*/
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -32,10 +55,11 @@ public class Snowball : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    void Flip()
     {
-        
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        facingRight = !facingRight;
     }
 }
